c747ddf8e829a5be80517aa386b63c0045aa0f43
Bryson Camp <bryson@bryson.camp>
2025-01-23 10:42:28 +1000

    Updates

---
+++ server/chunks/chunk-BoS_kHpr.js
@@ -0,0 +1,8 @@
+import { u as usePageContext } from "./chunk-DBC7D3kJ.js";
+function useData() {
+  const { data } = usePageContext();
+  return data;
+}
+export {
+  useData as u
+};
---
+++ server/entries/pages_about.mjs
@@ -0,0 +1,48 @@
+import { i as import1 } from "../chunks/chunk-DBC7D3kJ.js";
+import { jsxs, Fragment, jsx } from "react/jsx-runtime";
+/* empty css                       */
+import "react-dom/server";
+import "react";
+import "prop-types";
+/* empty css                       */
+/* empty css                       */
+import "vike/server";
+function Page() {
+  return /* @__PURE__ */ jsxs(Fragment, { children: [
+    /* @__PURE__ */ jsx("h1", { children: "About" }),
+    /* @__PURE__ */ jsx("p", { children: "Example of using Vike." })
+  ] });
+}
+const import2 = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProperty({
+  __proto__: null,
+  Page
+}, Symbol.toStringTag, { value: "Module" }));
+const configValuesSerialized = {
+  ["isClientRuntimeLoaded"]: {
+    type: "computed",
+    definedAtData: null,
+    valueSerialized: {
+      type: "js-serialized",
+      value: true
+    }
+  },
+  ["onRenderHtml"]: {
+    type: "standard",
+    definedAtData: { "filePathToShowToUser": "/renderer/+onRenderHtml.jsx", "fileExportPathToShowToUser": [] },
+    valueSerialized: {
+      type: "plus-file",
+      exportValues: import1
+    }
+  },
+  ["Page"]: {
+    type: "standard",
+    definedAtData: { "filePathToShowToUser": "/pages/about/+Page.jsx", "fileExportPathToShowToUser": [] },
+    valueSerialized: {
+      type: "plus-file",
+      exportValues: import2
+    }
+  }
+};
+export {
+  configValuesSerialized
+};
