2b8e102496613c2fc1a930290e992fe13ca2a7c5
Bryson Camp <bryson@bryson.camp>
2025-01-23 12:37:23 +1000

    Vike set-up

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import react from '@vitejs/plugin-react';
+import vike from 'vike/plugin';
+
+export default {
+  plugins: [
+    react(),
+    vike({
+      prerender: true  
+    })
+  ],
+  base: '/'
+};
